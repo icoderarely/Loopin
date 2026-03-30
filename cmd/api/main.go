15d3aad3baf50dc1830caf0cb1b918f56b4e925d
semicolon guy <itworksnavneet@gmail.com>
2026-03-31 00:35:35 +0530

    setup db

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/icoderarely/Loopin/internal/db"
 	"github.com/icoderarely/Loopin/internal/env"
 	"github.com/icoderarely/Loopin/internal/store"
 	"github.com/joho/godotenv"
@@ -14,11 +15,29 @@ func main() {
 		log.Fatal(" [error] loading environment variables")
 	}
 
-	store := store.NewPostgresStorage(nil)
-
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:pass@localhost/loopin?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
+
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer db.Close()
+	log.Println("db connection pool established")
+
+	store := store.NewPostgresStorage(db)
 
 	app := &application{
 		config: cfg,
