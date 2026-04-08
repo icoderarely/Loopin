c9749fd352d2e645b9c6cd084ad33cdae4517db5
semicolon guy <itworksnavneet@gmail.com>
2026-04-08 17:33:07 +0530

    fix: minor bugs

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -82,11 +82,10 @@ func (app *application) mount() http.Handler {
 			r.Group(func(r chi.Router) {
 				r.Get("/feed", app.getUserFeedHandler)
 			})
-
-			// Public routes
-			r.Route("/authentication", func(r chi.Router) {
-				r.Post("/user", app.registerUserHandler)
-			})
+		})
+		// Public routes
+		r.Route("/authentication", func(r chi.Router) {
+			r.Post("/user", app.registerUserHandler)
 		})
 	})
 
--- cmd/migrate/seed/main.go
+++ cmd/migrate/seed/main.go
@@ -19,5 +19,5 @@ func main() {
 
 	store := store.NewPostgresStorage(conn)
 
-	db.Seed(store)
+	db.Seed(store, conn)
 }
